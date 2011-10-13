907723b6284126742c993dc3137b07c06c2f87f8
makoto taniwaki <aa08109@cc.it-hiroshima.ac.jp>
2011-10-14 02:42:27 +0900

    add to convert to time frequency data. this is using gabor wavelet transform. Close #4 	modified:   utakata_wave.py

--- utakata_wave.py
+++ utakata_wave.py
@@ -4,6 +4,19 @@ import matplotlib.pyplot as plt
 #import scipy.io as sio
 #import scipy.linalg as slng
 
+import time
+def stopwatch(wrapped):
+  """計時デコレータ"""
+  def _wrapper(*args, **kwargs):
+    tic = time.time()
+    result = wrapped(*args, **kwargs)
+    toc = time.time()
+    doc = str(wrapped.__doc__).split("\n")[0]
+    print("[%s] %f[sec]" % (doc, toc - tic))
+    return result
+  return _wrapper
+
+
 #信号処理ハンドラ
 class BaseProcessHandler:
   """Base Class for Signal Process"""
@@ -51,5 +64,71 @@ class NormalizationWavedataHandler(BaseProcessHandler):
 
   def normalization(self):
     self.data = self.data / sp.absolute(self.data).max()
+
+class GaborwaveletWavedataHandler(BaseProcessHandler):
+  """Wave数列に対するハンドラ - ガボールウェーブレット変換"""
+  @stopwatch
+  def __init__(self, prevHandler):
+    """constructor at NormalizationWavedataHandler."""
+    BaseProcessHandler.__init__(self, prevHandler)
+    self.generateGaborMotherWavelet()
+    self.convertToTimeAndFrequencyData(85)
+
+  def generateGaborMotherWavelet(self):
+    pitch = 440.0
+    sigma = 6.
+    NL = 48.
+    NU = 39.
+    fs = 44100.
+    #asigma = 0.3
+    limit_t = 0.1
+    #zurashi = 1.
+
+    #NS = NL + NU + 1
+    f_t = sp.arange(-NL, NU+1)[:, sp.newaxis]
+    f = pitch * sp.power(2, f_t/11.)
+    sigmao = sigma*10**(-3)*sp.sqrt(fs/f)
+    t = sp.arange(-limit_t, limit_t+1/fs, 1/fs)
+
+    inv_sigmao = sp.power(sigmao, -1)
+    inv_sigmao_t = inv_sigmao * t
+    t_inv_sigmao2 = sp.multiply(inv_sigmao_t, inv_sigmao_t)
+    omega_t = 2*sp.pi*f*t
+    gabor = (1/sp.sqrt(2*sp.pi))
+    gabor = sp.multiply(gabor, sp.diag(inv_sigmao))
+    exps = -0.5*t_inv_sigmao2+sp.sqrt(-1)*omega_t
+    self.gabor = gabor*sp.exp(exps)
+
+  def convertToTimeAndFrequencyData(self, grain):
+    d = 10.
+    length = max(sp.shape(sp.arange(1, sp.size(self.data) - sp.size(self.gabor[1]), d)))
+    scale = sp.zeros((88, length))
+    datacapsule = sp.zeros((8821, grain))
+
+    # 行列を束ねて処理
+    #   個々にgabor*datadataを計算すると時間がかかる
+    #   一気にdatadataを束ねるとメモリ消費量が半端ない
+    #   よってdatadataに定義された数だけ束ねて処理
+    m = 0
+    datasize = sp.size(self.data) - sp.size(self.gabor[1])
+
+    for k in sp.arange(1, datasize+1, d*grain):
+      capsule_pointer = 0
+      endl = k+d*grain
+
+      if endl > datasize:
+        endl = k + datasize%(d*grain)
+
+      for l in sp.arange(k, endl, d):
+        datadata = self.data[l:l+sp.size(self.gabor[1])]
+        datacapsule[:, capsule_pointer] = datadata
+        capsule_pointer += 1
+
+      scale[:, m:m+grain] = sp.absolute(
+          sp.dot(self.gabor,datacapsule[:, :capsule_pointer]))
+      m += grain
+    
+    self.time_freq = scale
+
 
 
