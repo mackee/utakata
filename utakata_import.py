fee54a2167df188d5d29a0e74fd333dfd3297d00
makoto taniwaki <aa08109@cc.it-hiroshima.ac.jp>
2011-10-17 01:53:01 +0900

    modified import from wave file. add to specify sampling rate,

--- utakata_import.py
+++ utakata_import.py
@@ -1,18 +1,20 @@
 # -*- coding:utf8 -*-
 
 import scipy as sp
+import scipy.signal as ssig
 import wave
 
 # 入力ハンドラ
 class ImportWavedataHandler:
   """入力ハンドラ - waveファイルをインポートしてscipy数列に変換する"""
   
-  def __init__(self, filename):
+  def __init__(self, filename, fs=None):
     """constructor at ImportWaveHandler.
     
     :param filename: import file name::string
     """
     self.filename = filename
+    self.fs = fs
     self.data = self.original = self.importWave()
 
   def importWave(self):
@@ -21,6 +23,13 @@ class ImportWavedataHandler:
     waveframes = wf.readframes(wf.getnframes())
     self.framerate = wf.getframerate()
     self.duration = wf.getnframes() / self.framerate
-    return sp.fromstring(waveframes, sp.int16)
+    self.duration_list = sp.arange(0, self.duration, 1./self.fs)
+    data = sp.fromstring(waveframes, sp.int16)
+    if(self.fs == None):
+      self.fs = self.framerate
+    else:
+      data = ssig.resample(data, self.duration_list.size)
+
+    return data
 
 
