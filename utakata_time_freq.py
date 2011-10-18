0feaebbecce16daaaf788167f27bb30591c0652b
makoto taniwaki <aa08109@cc.it-hiroshima.ac.jp>
2011-10-19 03:01:53 +0900

    add feature that convert to MMML.

--- utakata_time_freq.py
+++ utakata_time_freq.py
@@ -99,7 +99,7 @@ class LogTimeFreqDataHandler(BaseProcessHandler):
     self.log()
 
   def log(self):
-    self.time_freq = sp.log(self.time_freq)
+    self.time_freq = sp.log10(self.time_freq)
 
 class CutOffNoiseTimeFreqDataHandler(BaseProcessHandler):
   """時間周波数データに対するハンドラ - テンポ情報をもとにノイズ除去"""
@@ -149,12 +149,157 @@ class CutOffNoiseTimeFreqDataHandler(BaseProcessHandler):
     return line
 
 
+class GradOnPitchTimeFreqDataHandler(BaseProcessHandler):
+  """時間周波数データに対するハンドラ - 音程が高くなるにつれて強調する"""
+  def __init__(self, prevHandler, factor):
+    BaseProcessHandler.__init__(self, prevHandler)
+    self.gradTimeFreq(factor)
+
+  def gradTimeFreq(self, factor):
+    tf = self.time_freq
+    time_freq = [tf[i, :]*(i**(1./factor)) for i in range(sp.shape(tf)[0])]
+    self.time_freq = sp.array(time_freq)
+
+
 class GenerateMMLTimeFreqDataHandler(BaseProcessHandler):
   """時間周波数データに対するハンドラ - MMLデータを生成"""
-  def __init__(self, prevHandler, minnotel=1./4.):
+  def __init__(self, prevHandler, target='time_freq', window=None, cut_num=40):
     BaseProcessHandler.__init__(self, prevHandler)
-    self.genMML()
+    self.genMML(getattr(self, target), window, cut_num)
 
-  def genMML(self):
-    pass
+  def genMML(self, target, window, cut_num):
+    self.interval_list_left = self.extractPhoneme(target, 0, cut_num, window)
+    self.interval_list_right = self.extractPhoneme(target, cut_num, 88, window)
 
+
+    fs = sp.shape(target)[1] / self.duration
+    self.mml_left = self.convertMMLList(self.interval_list_left, fs)
+    self.mml_right = self.convertMMLList(self.interval_list_right, fs)
+
+    self.mml_text_left = self.convertMMLText(self.mml_left)
+    self.mml_text_right = self.convertMMLText(self.mml_right)
+    print self.mml_text_left
+    print self.mml_text_right
+    
+  
+  def extractPhoneme(self, target, stkey, endkey, window):
+    target_other = sp.copy(target[stkey:endkey, :])
+    fs = sp.shape(target)[1] / self.duration
+    
+    if(window == None):
+      window = 1
+    else:
+      window = int(self.wavetempo / 60 * window * fs)
+    
+    before = target[:, 0]
+    interval_list = []
+    interval = interval_copy = []
+    before_interval = []
+    plength = 1
+    i = 1
+    while(i < sp.shape(target_other)[1]):
+      #for c in before_interval:
+      #  target_other[c, i] = 0
+      #  before[c] = 0
+      if(sp.array_equal(target_other[:, i], before)):
+        plength += 1
+        i += 1
+      else:
+        interval_list.append((interval, plength))
+        before_interval = interval_copy
+        plength = 1
+        freq_data = sp.zeros_like(target_other[:, i])
+        if(i+window > sp.shape(target_other)[1]):
+          window = sp.shape(target_other)[1] - i
+        for j in range(i, i+window):
+          freq_data = sp.logical_or(freq_data, target_other[:, j])
+        interval = self.analysisInterval(freq_data, stkey)
+        interval_copy = interval[:]
+        #delete overlap element
+        for element in before_interval:
+          if element in interval_copy:
+            interval.remove(element)
+        before = target_other[:, i]
+        i += window
+    return interval_list
+
+
+  def analysisInterval(self, freq_data, add_num):
+    interval = []
+    for i in range(sp.size(freq_data)):
+      if(freq_data[i] == 1):
+        interval.append(i+add_num)
+    return interval
+  
+  def convertMMLList(self, interval_list, fs):
+    note_name, note_length = self.generateNoteLength(self.wavetempo, fs)
+    note_scales = self.generateNoteScales()
+    mml_list = []
+    for interval in interval_list:
+      for name, length in zip(note_name, note_length):
+        if(interval[1] <= length[1] and interval[1] > length[0]):
+          scales = []
+          for scale in interval[0]:
+            scales.append(note_scales[scale])
+          if scales == []:
+            scales = ['r']
+          mml_list.append((scales, name))
+          break
+    return mml_list
+      
+
+
+  def generateNoteLength(self, tempo, fs):
+    length = (60. / tempo) * fs
+    note_length = sp.array([2**i for i in range(5)]) / 4.
+    note_length *= length
+    note_huten = sp.array(
+        [note_length[i-1]+note_length[i] for i in range(1, 5)])
+    note_length = sp.r_[note_length, note_huten]
+    note_length = sp.sort(note_length)
+
+    note_length_pair = []
+    for i in range(note_length.size):
+      try:
+        upper = (note_length[i+1] - note_length[i])/2
+        upper += note_length[i]
+      except IndexError:
+        upper = note_length[i] * 2
+      try:
+        lower = note_length_pair[-1][1]
+      except IndexError:
+        lower = 0
+      note_length_pair.append((lower, upper))
+        
+
+    note_name = ['16', '16.', '8', '8.', '4', '4.', '2', '2.', '1']
+    return (note_name, note_length_pair)
+
+  def generateNoteScales(self):
+    scales = ['a', 'a+', 'b', 'c', 'c+', 'd', 'd+', 'e', 'f', 'f+', 'g', 'g+']
+    scales = (scales * 9)[:88]
+    octaves = []
+    for i in range(9):
+      octaves += ([i]*12)
+    octaves = octaves[10:98]
+    return zip(scales, octaves)
+ 
+  def convertMMLText(self, mml_list):
+    text = ''
+    now_octave = 3
+    for elements in mml_list:
+      phoneme = ''
+      if(elements[0] == ['r']):
+        phoneme = 'r' + elements[1]
+      else:
+        for element in elements[0]:
+          if(element[1] < now_octave):
+            phoneme += '<' * (now_octave-element[1])
+            now_octave = element[1]
+          elif(element[1] > now_octave):
+            phoneme += '>' * (element[1]-now_octave)
+            now_octave = element[1]
+          phoneme += element[0] + '0'
+        phoneme = phoneme[:-1] + elements[1]
+      text += phoneme
+    return text
